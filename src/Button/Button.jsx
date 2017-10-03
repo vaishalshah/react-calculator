2fec020d113ea873e0f84ebd7d114ff3d2b36dcf
Shah <shvaisha@amazon.com>
2017-10-03 23:07:28 +0530

    new design applied

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -3,19 +3,18 @@ import './button.css';
 
 export default class Button extends React.Component {
   render() {
-    const isNumber =
-      (this.props.content >= 0 && this.props.content <= 9) ||
-      this.props.content === 'AC' ||
-      this.props.content === '.';
-    const cssClass = isNumber
-      ? 'calc-button col-md-3 calc-number'
-      : 'calc-button col-md-3';
+    let cssClass = 'calc-button ';
+    if (this.props.content === '(' || this.props.content === ')')
+      cssClass += 'calc-brackets';
+    else cssClass += 'col-md-12';
+    if (this.props.content === '=') cssClass += ' calc-equal';
+    const icon = <img src={this.props.icon} alt={this.props.content} />;
     return (
       <button
         className={cssClass}
         onClick={() => this.props.onClick(this.props.content)}
       >
-        {this.props.content}
+        {this.props.icon ? icon : this.props.content}
       </button>
     );
   }
--- src/Master/Master.jsx
+++ src/Master/Master.jsx
@@ -3,6 +3,12 @@ import math from 'mathjs';
 import './master.css';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
+import Division from '../icons/division.svg';
+import Multiplication from '../icons/multiplication.svg';
+import Delete from '../icons/delete.svg';
+import Plus from '../icons/plus.svg';
+import Minus from '../icons/minus.svg';
+import Equal from '../icons/equal.svg';
 
 export default class Master extends React.Component {
   constructor(props) {
@@ -25,6 +31,7 @@ export default class Master extends React.Component {
       '+',
       '-',
       '/',
+      '*',
       '.'
     ];
 
@@ -36,8 +43,8 @@ export default class Master extends React.Component {
   componentDidMount() {
     document.addEventListener('keydown', event => {
       if (this.validKeys.indexOf(event.key) > -1)
-        this.editEquation(event.key.replace('/', '÷'));
-      else if (event.key === 'Backspace') this.clearEquation();
+        this.editEquation(event.key.replace('/', '÷').replace('*', '×'));
+      else if (event.key === 'Backspace') this.clearEquation('D');
       else if (event.key === '=' || event.key === 'Enter') this.parseEquation();
     });
 
@@ -66,46 +73,66 @@ export default class Master extends React.Component {
     this.answerFlag = true;
     let answer = 'Invalid';
     try {
-      answer = math.eval(this.state.equation.replaceAll('÷', '/'));
+      answer = String(
+        math.eval(this.state.equation.replaceAll('÷', '/').replaceAll('×', '*'))
+      );
     } catch (err) {}
     this.setState({
       equation: answer
     });
   }
 
-  clearEquation() {
-    this.setState({
-      equation: '0'
-    });
+  clearEquation(action) {
+    if (action === 'C' || this.state.equation.length === 1 || this.answerFlag) {
+      this.setState({
+        equation: '0'
+      });
+    } else {
+      this.setState({
+        equation: this.state.equation.slice(0, -1)
+      });
+    }
   }
 
   render() {
     return (
       <div className="container calc-container">
         <Input text={this.state.equation} />
-        <div className="calc-row col-md-12">
-          <Button content="7" onClick={this.editEquation} />
-          <Button content="8" onClick={this.editEquation} />
-          <Button content="9" onClick={this.editEquation} />
-          <Button content="÷" onClick={this.editEquation} />
-        </div>
-        <div className="calc-row col-md-12">
-          <Button content="4" onClick={this.editEquation} />
-          <Button content="5" onClick={this.editEquation} />
-          <Button content="6" onClick={this.editEquation} />
-          <Button content="-" onClick={this.editEquation} />
-        </div>
-        <div className="calc-row col-md-12">
-          <Button content="1" onClick={this.editEquation} />
-          <Button content="2" onClick={this.editEquation} />
-          <Button content="3" onClick={this.editEquation} />
-          <Button content="+" onClick={this.editEquation} />
-        </div>
-        <div className="calc-row col-md-12">
-          <Button content="AC" onClick={this.clearEquation} />
-          <Button content="0" onClick={this.editEquation} />
-          <Button content="." onClick={this.editEquation} />
-          <Button content="=" onClick={this.parseEquation} />
+        <div className="calc-controls">
+          <div className="calc-col col-md-3">
+            <Button content="C" onClick={this.clearEquation} />
+            <Button content="7" onClick={this.editEquation} />
+            <Button content="4" onClick={this.editEquation} />
+            <Button content="1" onClick={this.editEquation} />
+            <div class="col-md-12 calc-brackets-div">
+              <Button content="(" onClick={this.editEquation} />
+              <Button content=")" onClick={this.editEquation} />
+            </div>
+          </div>
+          <div className="calc-col col-md-3">
+            <Button icon={Division} onClick={this.editEquation} content="÷" />
+            <Button content="8" onClick={this.editEquation} />
+            <Button content="5" onClick={this.editEquation} />
+            <Button content="2" onClick={this.editEquation} />
+            <Button content="0" onClick={this.editEquation} />
+          </div>
+          <div className="calc-col col-md-3">
+            <Button
+              icon={Multiplication}
+              onClick={this.editEquation}
+              content="×"
+            />
+            <Button content="9" onClick={this.editEquation} />
+            <Button content="6" onClick={this.editEquation} />
+            <Button content="3" onClick={this.editEquation} />
+            <Button content="." onClick={this.editEquation} />
+          </div>
+          <div className="calc-col col-md-3">
+            <Button icon={Delete} onClick={this.clearEquation} content="D" />
+            <Button icon={Plus} onClick={this.editEquation} content="+" />
+            <Button icon={Minus} onClick={this.editEquation} content="-" />
+            <Button icon={Equal} onClick={this.parseEquation} content="=" />
+          </div>
         </div>
       </div>
     );
